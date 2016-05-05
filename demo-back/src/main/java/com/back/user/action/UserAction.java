9b76c0c37a31e9f41dd85a6f880840b1dd1214d7
LeeJunTao <lijuntao363@qq.com>
2016-05-05 20:40:42 +0800

    测试

---
+++ demo-back/src/main/java/com/back/user/action/UserAction.java
@@ -0,0 +1,13 @@
+package com.back.user.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by lijuntao on 2016/5/5.
+ */
+@Controller
+@RequestMapping("/user")
+public class UserAction {
+
+}
